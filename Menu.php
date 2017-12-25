b3beae0e2247ca9d9a81b1bf035cc91988fdadd8
Rafael Zelaya <rafaelzelayax@gmail.com>
2017-12-25 12:59:50 -0600

    Se agrego al menú el punto de venta.

--- Menu.php
+++ Menu.php
@@ -3,10 +3,12 @@
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
         <li><a href="Barbero.php">Barberos</a></li>
+        <li><a href="PuntoVenta.php">Punto de venta</a></li>
         <li><a href="Login.php">Cerrar</a></li>
       </ul>
       <ul id="nav-mobile" class="side-nav">
         <li><a href="Barbero.php">Barberos</a></li>
+        <li><a href="PuntoVenta.php">Punto de venta</a></li>
         <li><a href="Login.php">Cerrar</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
--- Vistas/PuntoVenta.php
+++ Vistas/PuntoVenta.php
@@ -2,15 +2,11 @@
 <html lang="en">
 <?php include '../Cabecera.php'; ?>
 <body>
- <?//php include "../Menu.php";?>
-   <div class="black lighten-2 nav-wrapper center">
-     <a href="#" class="brand-logo center">
-       <img src = "../img/bannerNegro.jpg"  class="responsive-img logo"/>
-     </a>
-   </div>
+ <?php include "../Menu.php";?>
+
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
-      <h1 class="titulo center black-text">Punto de Venta</h1>
+      <h1 class="header center black-text">Punto de Venta</h1>
       <div class="row center">
           Mostrar aquí los servicios como corte...
       </div>
