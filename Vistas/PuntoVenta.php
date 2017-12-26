b64c1f5544037f9a8e9801e0a4016a3172770aa8
Rafael Zelaya <rafaelzelayax@gmail.com>
2017-12-26 12:09:33 -0600

    Se agrego manualmente la lista de servicios. Hay que hacerlo automatico.

--- Vistas/PuntoVenta.php
+++ Vistas/PuntoVenta.php
@@ -9,8 +9,39 @@
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <h1 class="header center black-text">Punto de Venta</h1>
-      <div class="row center">
-          Mostrar aquí los servicios como corte...
+      <div class="row">
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="../img/CorteCabello.jpg" alt="" class="circle">
+            <span class="title">Corte de cabello</span>
+            <p>$5.00<p>
+              <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../img/CorteBarba.png" alt="" class="circle">
+            <span class="title">Corte de barba</span>
+            <p>$5.00</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../img/PerfiladoBarba.png" alt="" class="circle">
+            <span class="title">Perfilado de barba</span>
+            <p>$5.00</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../img/PerfiladoCeja.png" alt="" class="circle">
+            <span class="title">Perfilado de cejas</span>
+            <p>$5.00</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+          <li class="collection-item avatar">
+            <img src="../img/facial.png" alt="" class="circle">
+            <span class="title">Facial</span>
+            <p>$5.00</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+</ul>
       </div>
     </div>
   </div>
