aff24e9130c72b1c8eee4064f8f8cca39c1e58f8
Rafael Zelaya <rafaelzelayax@gmail.com>
2018-01-22 21:15:02 -0600

    Vista de reportes.

--- Vistas/Reportes.php
+++ Vistas/Reportes.php
@@ -12,6 +12,15 @@
      <div class="row" id = "collectionId">No existen reportes disponibles</div>
    </div>
  </div>
+select barberos.Nombres as barbero, tbl_serviciosproductos.nombre as servicio,
+count(detalle_factura.id_servicio) as cantidad, detalle_factura.id_factura,
+detalle_factura.id_servicio,factura.Fecha from detalle_factura
+inner join barberos on barberos.Id = detalle_factura.id_barbero
+inner join tbl_serviciosproductos on tbl_serviciosproductos.id =
+detalle_factura.id_servicio
+inner join factura on factura.id = detalle_factura.id_factura
+where factura.fecha BETWEEN '2018-01-16' and '2018-01-17'
+group by tbl_serviciosproductos.nombre,tbl_serviciosproductos.id,barberos.Nombres
  <?php include "../footer.php"?>
  </body>
 </html>
